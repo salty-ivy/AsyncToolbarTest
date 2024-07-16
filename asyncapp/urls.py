40788618a4eb8b9913f8acbccafcdc7ab4591145
Aman Pandey <aman2001mi@gmail.com>
2024-07-17 03:51:44 +0530

    comment unnecessary logging

--- asyncapp/urls.py
+++ asyncapp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from asyncapp.views import CarListView, get_car_list
+from asyncapp.views import CarListView, get_car_list, sleep_view, sleep_view_small, new_async_view
 
 app_name = "asyncapp"
 
 urlpatterns = [
     path('cars/', CarListView.as_view(), name='car-list'),
+    path('sleep/', sleep_view, name='sleep'),
+    path('sleep-small/', sleep_view_small, name='sleep'),
+
+    # async views
     path('async-cars/', get_car_list, name='async-cars'),
+    path('async-small/', new_async_view, name='async-small'),
+
 ]
 
--- asyncapp/views.py
+++ asyncapp/views.py
@@ -1,7 +1,14 @@
-from django.http import JsonResponse
+import asyncio
+
+from django.http import HttpResponse
 from django.views.generic import ListView
 from django.shortcuts import render
 from asyncapp.models import Car
+import time
+from contextvars import copy_context
+import os
+import threading
+from django.db import connections
 
 
 class CarListView(ListView):
@@ -12,8 +19,43 @@ class CarListView(ListView):
     model = Car
     template_name = 'cars/car_list.html'
 
+    def get(self, request, *args, **kwargs):
+        # print("DB connections in SYNC view***:", id(connections.all()[0]))
+        print("context in SYNC car view***: ", id(copy_context()))
+        print("thread_id in SYNC car view***: ", threading.get_ident())
+        return super().get(self, request, *args, **kwargs)
+
+
+def sleep_view(request):
+    time.sleep(10)
+    print(os.getpid())
+    print("context in view***: ", id(copy_context()))
+    return HttpResponse(f"sleep for 10 seconds {id(copy_context())}, thread_id: {threading.get_ident()}")
+
+
+def sleep_view_small(request):
+    time.sleep(1)
+    print(os.getpid())
+    print("context in view***: ", id(copy_context()))
+    return HttpResponse(f"sleep for 1 seconds {id(copy_context())}, thread_id: {threading.get_ident()}")
+
 
 async def get_car_list(request):
     cars = Car.objects.all()
-    context = {'object_list': "cars list"}
-    return render(request, 'cars/car_list.html', context)
+    # print("DB connections in ASYNC view***:", id(connections.all()[0]))
+    # print("djdt_logger in connection: ", connections.all()[0]._djdt_logger or "None")
+    context = {'object_list': "car list"}
+    print("context in ASYNC car view***: ", id(copy_context()))
+    print("thread_id in ASYNC car view***: ", threading.get_ident())
+    # await asyncio.sleep(5)
+    rendered = render(request, 'cars/car_list_async.html', context)
+    return rendered
+
+
+async def new_async_view(request):
+    cars = Car.objects.all()
+    context = {'object_list': "car list"}
+    print("context in ASYNC car view***: ", id(copy_context()))
+    print("thread_id in ASYNC car view***: ", threading.get_ident())
+    await asyncio.sleep(1)
+    return render(request, 'cars/car_list_async.html', context)
